68e15c491ccd7eea3884cd91cb3047dcc50cd2f2
clay <vancuong196@gmail.com>
2025-03-23 21:27:22 +0700

    Support custom fonts

--- src/org/recompile/mobile/PlatformFont.java
+++ src/org/recompile/mobile/PlatformFont.java
@@ -20,6 +20,7 @@ import javax.microedition.lcdui.Font;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.awt.font.TextAttribute;
 
 import java.util.Hashtable;
@@ -31,23 +32,52 @@ public class PlatformFont
 
 	public java.awt.Font awtFont;
 
+	private static final String FONT_REGULAR = "freej2me_system" + File.separatorChar + "fonts" + File.separatorChar + "font_regular.ttf";
+	private static final String FONT_BOLD = "freej2me_system" + File.separatorChar + "fonts" + File.separatorChar + "font_bold.ttf";
+	private static final String FONT_ITALIC = "freej2me_system" + File.separatorChar + "fonts" + File.separatorChar + "font_italic.ttf";
+	private static class FontLoader {
+		private static final Map<String, java.awt.Font> loadedFonts = new Hashtable<>();
+
+		private FontLoader() {
+		}
+
+		public static java.awt.Font getFont(String fontFilePath, int size) throws Exception {
+			if (loadedFonts.containsKey(fontFilePath)) {
+				return loadedFonts.get(fontFilePath).deriveFont(size);
+			}
+			java.awt.Font font = java.awt.Font.createFont(java.awt.Font.TRUETYPE_FONT, new java.io.File(fontFilePath));
+			loadedFonts.put(fontFilePath, font);
+			return font.deriveFont((float) size);
+		}
+	}
+
 	public PlatformFont(Font font)
 	{
-		// We'll use SansSerif for both SYSTEM and PROPORTIONAL
-		String fontFace = java.awt.Font.SANS_SERIF;
-		if(font.getFace() == Font.FACE_MONOSPACE) { fontFace = java.awt.Font.MONOSPACED; }
+		int size = font.getPointSize();
+		int style = font.getStyle();
+		
+		try {
+			if(font.getFace() == Font.FACE_MONOSPACE) { 
+				awtFont = new java.awt.Font( java.awt.Font.MONOSPACED, font.getStyle(), font.getPointSize());
+			} else if (style == java.awt.Font.BOLD) {
+				awtFont = FontLoader.getFont(FONT_BOLD, size);
+			} else if (style == java.awt.Font.ITALIC) {
+				awtFont = FontLoader.getFont(FONT_ITALIC, size);
+			} else 
+			{
+				awtFont = FontLoader.getFont(FONT_REGULAR, size);
+			}
+		} catch (Exception e) {
+			awtFont = new java.awt.Font(java.awt.Font.SANS_SERIF, style, size);
+		}
 
-		awtFont = new java.awt.Font(fontFace, font.getStyle(), font.getPointSize());
-
-		// Standard java doesn't handle underlining the same way, so do it here
 		if((font.getStyle() & Font.STYLE_UNDERLINED) > 0)
 		{
 			Map<TextAttribute, Object> map = new Hashtable<TextAttribute, Object>(1);
 			map.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
-
 			awtFont = awtFont.deriveFont(map);
 		}
-
+		
 		gc = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB).createGraphics();
 		gc.setFont(awtFont);
 	}
